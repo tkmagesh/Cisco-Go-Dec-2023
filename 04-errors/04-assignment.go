c80f4cf80ae2346e3977a47c560713d728582a6f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-07 10:16:08 +0530

    errors - assignment

---
+++ 04-errors/04-assignment.go
@@ -0,0 +1,72 @@
+/*
+Refactor the below program using functions
+Make sure that every function has ONLY ONE reason to change (SRP)
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, n1, n2, result int
+	for {
+		userChoice = getUserChoice()
+
+		if userChoice == 5 {
+			break
+		}
+		// remove the following if block by using errors
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+		oper := getOperation(userChoice)
+		n1, n2 = getOperands()
+		result = oper(n1, n2)
+		fmt.Println("Result :", result)
+	}
+}
+
+func getUserChoice() int {
+	var userChoice int
+	fmt.Printf("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit\n")
+	fmt.Println("Enter your choice")
+	fmt.Scanln(&userChoice)
+	return userChoice
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter the operands")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
+
+// return an error if the userChoice is not 1 - 4
+func getOperation(userChoice int) func(int, int) int {
+	switch userChoice {
+	case 1:
+		return add
+	case 2:
+		return subtract
+	case 3:
+		return multiply
+	default:
+		return divide
+	}
+}
