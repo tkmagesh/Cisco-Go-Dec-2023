4bfaf25aa7be8520e04b7a479aa31a65f6c51391
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-07 16:51:30 +0530

    Day-02 complete

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 
 	"github.com/fatih/color"
-	"github.com/tkmagesh/cisco-go-dec-2023/09-modularity/calculator"
+	// "github.com/tkmagesh/cisco-go-dec-2023/09-modularity/calculator"
+	calc "github.com/tkmagesh/cisco-go-dec-2023/09-modularity/calculator"
 )
 
 func main() {
 	color.Red("modularity app executed")
 
-	fmt.Println(calculator.Add(100, 200))
-	fmt.Println(calculator.Subtract(100, 200))
-	fmt.Println(calculator.Multiply(100, 200))
-	fmt.Println("operation count :", calculator.OpCount())
+	/*
+		fmt.Println(calculator.Add(100, 200))
+		fmt.Println(calculator.Subtract(100, 200))
+		fmt.Println(calculator.Multiply(100, 200))
+		fmt.Println("operation count :", calculator.OpCount())
+	*/
+
+	fmt.Println(calc.Add(100, 200))
+	fmt.Println(calc.Subtract(100, 200))
+	fmt.Println(calc.Multiply(100, 200))
+	fmt.Println("operation count :", calc.OpCount())
 
 }
