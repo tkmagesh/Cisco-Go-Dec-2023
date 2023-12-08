822edd166b7d6f7a8d58a8d3a42e73ef0eaf0ac2
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-08 16:39:36 +0530

    streaming assignment solution

--- 14-concurrency/18-demo.go
+++ 14-concurrency/18-demo.go
@@ -1,10 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 func main() {
-	// receive the generated fibonocci series and print them
+	ch := genFib()
+	for no := range ch {
+		fmt.Println(no)
+	}
+	fmt.Println("Done")
 }
 
 // update this function to generate fibonocci series ( rand.Intn(20) count )
-func genFib() {
-
+func genFib() <-chan int {
+	ch := make(chan int)
+	go func() {
+		for x, y, count := 0, 1, rand.Intn(20); count >= 0; count-- {
+			ch <- x
+			time.Sleep(500 * time.Millisecond)
+			x, y = y, x+y
+		}
+		close(ch)
+	}()
+	return ch
 }
