fd4a43a4524a16fffed4c615773da1d55a300e29
Magesh Kuppan <tkmagesh77@gmail.com>
2023-12-08 16:22:55 +0530

    concurrency - assignment

---
+++ 14-concurrency/18-demo.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	// receive the generated fibonocci series and print them
+}
+
+// update this function to generate fibonocci series ( rand.Intn(20) count )
+func genFib() {
+
+}
